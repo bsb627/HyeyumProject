1617da497ae5ac41dda657f27503da91b3f97250
bsb627 <beom3220@gmail.com>
2021-04-06 15:52:22 +0900

    show service crud 메소드 생성

--- HyeYum/src/show/model/service/ShowService.java
+++ HyeYum/src/show/model/service/ShowService.java
@@ -1,5 +1,80 @@
 package show.model.service;
 
-public class ShowService {
+import common.JDBCTemplate;
+import show.model.vo.ShowInfo;
 
+public class ShowService {
+	private JDBCTemplate factory;
+	
+	public ShowService() {
+		factory = JDBCTemplate.getConnection();
+	}
+	
+	public ShowInfo printShowInfo() {
+		ShowInfo info = null;
+		return info;
+	}
+	
+	public int modifyShowInfo() {
+		int result = 0;
+		return result;
+	}
+	
+	public ShowInfo insertShowInfo() {
+		
+		ShowInfo info = null;
+		
+		return info;
+	}
+	
+	public int deleteShowInfo() {
+		int result = 0;
+		return result ;
+	}
+	
+	public ShowInfo printShowReview() {
+		ShowInfo info = null;
+		return info;
+	}
+	
+	public int modifyShowReview() {
+		int result = 0;
+		return result;
+	}
+	
+	public ShowInfo insertShowReview() {
+		
+		ShowInfo info = null;
+		
+		return info;
+	}
+	
+	public int deleteShowReview() {
+		int result = 0;
+		return result ;
+	}
+	
+	public ShowInfo printShowReviewReply() {
+		ShowInfo info = null;
+		return info;
+	}
+	
+	public int modifyShowReviewReply() {
+		int result = 0;
+		return result;
+	}
+	
+	public ShowInfo insertShowReviewReply() {
+		
+		ShowInfo info = null;
+		
+		return info;
+	}
+	
+	public int deleteShowReviewReply() {
+		int result = 0;
+		return result ;
+	}
+	
+	
 }
--- HyeYum/src/show/model/vo/ShowReview.java
+++ HyeYum/src/show/model/vo/ShowReview.java
@@ -3,7 +3,7 @@ package show.model.vo;
 import board.model.vo.Board;
 
 public class ShowReview extends Board{ // 공연 게시판
-	
+	private int contentsNo; // 공연번호
 	private String fileName; // 이미지파일
 	private String snsLink; // sns링크주소
 	private String ticketNumber; // 예매 일련 번호
@@ -11,6 +11,17 @@ public class ShowReview extends Board{ // 공연 게시판
 	public ShowReview() {
 		// TODO Auto-generated constructor stub
 	}
+
+	
+	public int getContentsNo() {
+		return contentsNo;
+	}
+
+
+	public void setContentsNo(int contentsNo) {
+		this.contentsNo = contentsNo;
+	}
+
 
 	public String getFileName() {
 		return fileName;
@@ -38,7 +49,7 @@ public class ShowReview extends Board{ // 공연 게시판
 
 	@Override
 	public String toString() {
-		return "ShowReview [no=" + super.no + ", title=" + super.title + ", contents=" + super.contents +", nick=" + super.nick + ", hits=" + super.hits+" likes=" + super.likes + ", enrollDate=" + super.enrollDate + "fileName=" + this.fileName + ", snsLink=" + this.snsLink + ", ticketNumber=" + this.ticketNumber + "]";
+		return "ShowReview [no=" + super.no + ", title=" + super.title + ", contents=" + super.contents +", nick=" + super.nick + ", hits=" + super.hits+" likes=" + super.likes + ", enrollDate=" + super.enrollDate + "fileName=" + this.fileName + "contentsNo=" + this.contentsNo + ", snsLink=" + this.snsLink + ", ticketNumber=" + this.ticketNumber + "]";
 	}
 	
 	
