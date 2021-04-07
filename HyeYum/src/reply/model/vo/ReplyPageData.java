c1e87749462ecdd1d19c81c1c1db60ae0359575c
bsb627 <beom3220@gmail.com>
2021-04-07 23:14:07 +0900

    show 페이징 추가

---
+++ HyeYum/src/reply/model/vo/ReplyPageData.java
@@ -0,0 +1,14 @@
+package reply.model.vo;
+
+import java.util.ArrayList;
+
+public class ReplyPageData {
+	
+	private ArrayList<Reply> replyList;
+	private String pageNavi;
+
+	public ReplyPageData() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ HyeYum/src/show/model/vo/ShowPageData.java
@@ -0,0 +1,15 @@
+package show.model.vo;
+
+import java.util.ArrayList;
+
+public class ShowPageData {
+	
+	private ArrayList<ShowInfo> showList;
+	private ArrayList<ShowReview> reviewList;
+	private String pageNavi;
+
+	public ShowPageData() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
