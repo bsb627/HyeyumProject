4295312bf95193f78b4a8db563eda95b8e7fd2b7
user <user@DESKTOP-4L8UGOJ>
2021-04-08 16:43:43 +0900

    servlet명 변경

--- HyeYum/src/movie/controller/MovieInfoModifyServlet.java
+++ HyeYum/src/movie/controller/MovieInfoModifyServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MovieContentsModifyServlet
  */
-@WebServlet("/movieinfo/modify")
+@WebServlet("/movieInfo/modify")
 public class MovieInfoModifyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
--- HyeYum/src/movie/controller/ReviewPlusLikesCountServlet.java
+++ HyeYum/src/movie/controller/ReviewPlusLikesCountServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AddHitsCountServlet
  */
-@WebServlet("/movieReview/likesCount")
+@WebServlet("/movieReview/plusLikesCount")
 public class ReviewPlusLikesCountServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
