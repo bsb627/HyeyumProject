f98264903aba0415456539b26edb974fd685e80f
bsb627 <beom3220@gmail.com>
2021-04-08 10:35:33 +0900

    show, reply, order UML 추가

---
+++ HyeYum/src/order/model/vo/OrderPageData.java
@@ -0,0 +1,14 @@
+package order.model.vo;
+
+import java.util.ArrayList;
+
+public class OrderPageData {
+	
+	private ArrayList<Order> orderList;
+	private String pageNavi;
+
+	public OrderPageData() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
