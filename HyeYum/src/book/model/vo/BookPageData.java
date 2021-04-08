64d15b27610cc8657787dde519d58e3c0f5edffa
icicl <icicl@DESKTOP-J04QA0S>
2021-04-08 10:33:01 +0900

    BookpageData 수정

--- HyeYum/src/book/model/vo/BookPageData.java
+++ HyeYum/src/book/model/vo/BookPageData.java
@@ -2,13 +2,11 @@ package book.model.vo;
 
 import java.util.ArrayList;
 
-import show.model.vo.ShowInfo;
-import show.model.vo.ShowReview;
 
 public class BookPageData {
 
-	private ArrayList<ShowInfo> showList;
-	private ArrayList<ShowReview> reviewList;
+	private ArrayList<BookInfo> infoList;
+	private ArrayList<BookReview> reviewList;
 	private String pageNavi;
 
 	public BookPageData() {
