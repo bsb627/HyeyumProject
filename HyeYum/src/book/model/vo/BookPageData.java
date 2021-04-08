dc389a96c7f63fae11c9051bd359774d4e652c73
icicl <icicl@DESKTOP-J04QA0S>
2021-04-08 10:23:48 +0900

    Book search 추가 Book FileData 추가

---
+++ HyeYum/src/book/model/vo/BookPageData.java
@@ -0,0 +1,17 @@
+package book.model.vo;
+
+import java.util.ArrayList;
+
+import show.model.vo.ShowInfo;
+import show.model.vo.ShowReview;
+
+public class BookPageData {
+
+	private ArrayList<ShowInfo> showList;
+	private ArrayList<ShowReview> reviewList;
+	private String pageNavi;
+
+	public BookPageData() {
+		// TODO Auto-generated constructor stub
+	}
+}
