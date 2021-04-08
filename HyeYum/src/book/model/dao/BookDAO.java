5654d646624e0a1c33914fbdad49e0d8589c6aa7
icicl <icicl@DESKTOP-J04QA0S>
2021-04-08 15:57:50 +0900

    Book매개변수 추가

--- HyeYum/src/book/model/dao/BookDAO.java
+++ HyeYum/src/book/model/dao/BookDAO.java
@@ -1,5 +1,6 @@
 package book.model.dao;
 
+import java.sql.Connection;
 import java.util.ArrayList;
 
 import book.model.vo.BookInfo;
@@ -8,100 +9,110 @@ import book.model.vo.BookShare;
 
 public class BookDAO {
 	//BookInfo
-	public ArrayList<BookInfo> selectAllBookInfo(){
-		
+	public ArrayList<BookInfo> selectAllBookInfo(Connection conn, int currentPage){ // 책정보 전체보기
+		return null;
+	}
+	public BookInfo selectOneBookInfo(Connection conn, int showNo) { // 책정보 상세보기
 		
 		return null;
 	}
-	public BookInfo selectOneBookInfo() {
-		
-		return null;
-	}
-	public int insertBookInfo() {
-		
+	public int insertBookInfo(Connection conn, BookInfo bookInfo) { // 책정보 등록
 		return 0;
 	}
-	public int updateBookInfo() {
-		
+	public int updateBookInfo(Connection conn, BookInfo bookInfo) { // 책정보 수정
 		return 0;
 	}
-	public int deleteBookInfo() {
-		
+	public int deleteBookInfo(Connection conn, int BookNo) { // 책정보 삭제
 		return 0;
 	}
 	
 	// BookReview
-	public ArrayList<BookReview> selectAllBookReview(){
+	public ArrayList<BookReview> selectAllBookReview(Connection conn, int currentPage){ // 책리뷰 전체보기
+		return null;
+	}
+	public BookReview selectOneBookReview(Connection conn, int showNo) { //책리뷰 상세보기
 		
 		return null;
 	}
-	public BookReview selectOneBookReview() {
-		
+	public int insertBookReview(Connection conn, BookReview review) { // 책리뷰 등록
+		return 0;
+	}
+	public int updateBookReview(Connection conn, BookReview review) { // 책리뷰 수정
+		return 0;
+	}
+	public int deleteBookReview(Connection conn, int reviewNo) { // 책리뷰 삭제
+		return 0;
+	}
+	public String getReviewPageNavi(Connection conn, int currentPage) { // 책리뷰 페이징
 		return null;
 	}
-	public int insertBookReview() {
-		
+	public int totalReviewCount(Connection conn) { //책리뷰 총 게시글 수
 		return 0;
 	}
-	public int updateBookReview() {
-		
+	public ArrayList<BookReview> selectSearchReviewList(Connection conn, int currentPage, String searchCategory){ // 책리뷰 검색결과 전체보기
+		return null;
+	}
+	public String getSearchReviewPageNavi(Connection conn, int currentPage, String search, String searchCategory) { // 책리뷰 검색 페이징
+		return null;
+	}
+	public int searchTotalReviewCount(Connection conn, String search, String searchCategory) { // 책리뷰 검색
 		return 0;
 	}
-	public int deleteBookReview() {
-		
+	public int updateHitsReview(Connection conn, int bookNo) { //  게시글 조회수 증가
 		return 0;
 	}
-	
+	public int insertLikesReview(Connection conn, int bookNo, String userId) { // 게시글 좋아요 등록
+		return 0;
+	}
+	public int updateLikesReview(Connection conn, int showNo, String userId) { // 좋아요 취소
+		return 0;
+	}
 	// BookShare
-	public ArrayList<BookShare> selectAllBookShare(){
+	public ArrayList<BookShare> selectAllBookShare(Connection conn, int currentPage){ // 책나눔 전체보기
 		
 		return null;
 	}
-	public BookShare selectOneBookShare() {
+	public BookShare selectOneBookShare(Connection conn, int showNo) { // 책나눔 상세보기
 		
 		return null;
 	}
-	public int insertBookShare() {
+	public int insertBookShare(Connection conn, BookShare share) { // 책나눔 등록
 		
 		return 0;
 	}
-	public int updateBookShare() {
+	public int updateBookShare(Connection conn, BookShare share) { // 책나눔 수정
 		
 		return 0;
 	}
-	public int deleteBookShare() {
+	public int deleteBookShare(Connection conn, int shareNo) { // 책나눔 삭제
 		
 		return 0;
 	}
-	
-	
-	
-	public String getPageNavi() {
-		
+	public String getSharePageNavi(Connection conn, int currentPage) { // 책나눔 페이징
 		return null;
 	}
-	public String getSearchPageNavi() {
-		
-		return null;
-	}
-	public int totalCount() {
+	public int totalShareCount(Connection conn) { // 책나눔 총 게시글 수
 		
 		return 0;
 	}
-	public int searchTotalCount() {
+	public ArrayList<BookShare>selectSearchBookShare(Connection conn, int currentPage, String search, String searchCategory){
 		
+		return null;
+	}
+	public String getSearchSharePageNavi(Connection conn, int currentPage, String search, String searchCategory) { // 책나눔 검색 페이징
+		
+		return null;
+	}
+	public int searchTotalShareCount(Connection conn, String search, String searchCategory ) {
 		return 0;
 	}
-	public ArrayList<BookInfo> selectSearchBookInfo(){
-		
-		return null;
+	public int  updateHitsShare(Connection conn, int shareNo) { // 조회수
+		return 0;
 	}
-	public ArrayList<BookReview>selectSearchBookReview(){
-		
-		return null;
+	public int insertLikesReview(Connection conn, int shareNo) { // 좋아요 등록
+		return 0;
 	}
-	public ArrayList<BookShare>selectSearchBookShare(){
-		
-		return null;
+	public int updateLikesShare(Connection conn, int shareNo, String userId) { //좋아요 취소
+		return 0;
 	}
 }
--- HyeYum/src/book/model/service/BookService.java
+++ HyeYum/src/book/model/service/BookService.java
@@ -1,10 +1,13 @@
 package book.model.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import book.model.vo.BookInfo;
+import book.model.vo.BookPageData;
 import book.model.vo.BookReview;
 import book.model.vo.BookShare;
 import common.JDBCTemplate;
-import show.model.vo.ShowInfo;
 
 
 public class BookService {
@@ -16,81 +19,127 @@ public class BookService {
 	}
 	
 	// BookInfo
-	public BookInfo printAllBookInfo() {
+	public BookPageData printAllBookInfo(int currentPage) { // 책정보 목록+페이징
+		Connection conn = null;
+		BookPageData pd = new BookPageData();
+		try {
+			conn = factory.createConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCTemplate.close(conn);
+		}
+		return pd;
+	}
+	
+	public BookInfo printOneBookInfo(int bookNo) { // 책정보 상세보기
 		BookInfo info = null;
 		return info;
 	}
-	public BookInfo printOneBookInfo() {
-		
-		return null;
-	}
-	public int modifyBookInfo() {
+	public int modifyBookInfo(BookInfo bookInfo) { // 책정보 수정
 		int result = 0;
 		return result;
 	}
-	public int registerBookInfo() {
-		BookInfo info = null;
-		return 0;
+	public int registerBookInfo(BookInfo bookInfo) { //책정보 등록
+		int result = 0;
+		return result;
 	}
-	public int deleteBookInfo() {
+	public int deleteBookInfo(int bookNo) { //책정보 삭제
 		int result = 0;
 		return result ;
 	}
-	public int BookInfoHits() {
-		
-		return 0;
-	}
-	public int BookInfoLikes() {
-		
-		return 0;
-	}
+
 	// BookReview
-	public BookReview printAllBookReview() {
+	public BookPageData printAllBookReview(int currentPage) { // 책리뷰 목록 + 페이징
+		Connection conn = null;
+		BookPageData pd = new BookPageData();
+		
+		try {
+			conn = factory.createConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(conn);
+		}
+		return pd;
+	}
+	public BookReview printOneBookReview() {//책리뷰 상세보기
 		BookReview review = null;
+				
 		return review;
 	}
-	public BookReview printOneBookReview() {
-		
-		return null;
-	}
-	public int modifyBookReview() {
+	public int modifyBookReview() { //책리뷰 수정
 		int result = 0;
 		return result;
 	}
-	public int registerBookReview() {
-		
-		return 0;
+	public int registerBookReview() { //책리뷰 등록
+		int result = 0;
+		return result;
 	}
-	public int deleteBookReview() {
+	public int deleteBookReview() { //책리뷰 삭제
 		int result = 0;
 		return result ;
 	}
-	public int BookReviewHits() {
+	
+	public BookPageData seachPrintAllBookReview(int currentPage, String search, String searchCategory) { // 책리뷰 검색결과
+		Connection conn = null;
+		BookPageData pd = new BookPageData();
 		
-		return 0;
+		try {
+			conn = factory.createConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCTemplate.close(conn);
+		}
+		return pd;
 	}
-	public int BookReviewLikes() {
-		
-		return 0;
+	
+	public int BookReviewHits(int BookNo) {// 조회수 증가
+		int result=0;
+		return result;
+	}
+	public int BookReviewPlusLikes(String userId, int BookNo) {
+		int result = 0;
+		return result;
+	}
+	public int BookReviewMinusLikes(String userId, int BookNo) {
+		int result = 0;
+		return result;
 	}
 	
 	// BookShare
-	public BookShare printAllBookShare() {
-		BookShare review = null;
-		return review;
+	public BookPageData printAllBookShare(int currentPage) {// 책나눔 목록 + 페이징
+		Connection conn = null;
+		BookPageData pd = new BookPageData();
+		
+		try {
+			conn = factory.createConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCTemplate.close(conn);
+		}
+		return pd;
 	}
-	public BookShare printOneBookShare() {
-		return null;
+	
+	public BookShare printOneBookShare(int bookNo) { // 책나눔 상세보기
+		BookShare share = null;
+		return share;
 	}
-	public int registerBookShare() {
-
-		return 0;
-	}
-	public int modifyBookShare() {
+	public int registerBookShare(BookShare share) { // 책나눔 등록
 		int result = 0;
 		return result;
 	}
-	public int deleteBookShare() {
+	public int modifyBookShare(BookShare share) { //책나눔 수정
+		int result = 0;
+		return result;
+	}
+	public int deleteBookShare(int bookNo) { // 책나눔 삭제
 		int result = 0;
 		return result ;
 	}
