5c126f82e81d9b6b5f2718d8f88e569cb4e6f2dd
bsb627 <beom3220@gmail.com>
2021-04-09 11:23:23 +0900

    서블릿 오류 수정

--- HyeYum/src/file/controller/FileUploadServlet.java
+++ HyeYum/src/file/controller/FileUploadServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class FileData
  */
-@WebServlet("/file/download")
+@WebServlet("/file/upload")
 public class FileUploadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
--- HyeYum/src/message/controller/MsgSearchServlet.java
+++ HyeYum/src/message/controller/MsgSearchServlet.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class MessageSerachServlet
  */
 @WebServlet("/message/search")
-public class MsgSerachServlet extends HttpServlet {
+public class MsgSearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MsgSerachServlet() {
+    public MsgSearchServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
