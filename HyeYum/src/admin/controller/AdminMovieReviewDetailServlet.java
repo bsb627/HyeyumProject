cd1fceaa4d31f8d73f34d581003316c71425f9bc
bsb627 <beom3220@gmail.com>
2021-04-18 19:27:19 +0900

    서블렛명 겹치는거 수정

--- HyeYum/src/admin/controller/AdminMovieReviewDetailServlet.java
+++ HyeYum/src/admin/controller/AdminMovieReviewDetailServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AdminMovieReviewDetailServlet
  */
-@WebServlet("/admin/recommend/detail")
+@WebServlet("/admin/movie/detail")
 public class AdminMovieReviewDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
