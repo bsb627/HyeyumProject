bda923e4c0557e54defcbceba0631a9a7cb73089
icicle_4 <dwayne.han4@gmail.com>
2021-04-19 16:12:05 +0900

    내가 작성한 글 수정

--- HyeYum/src/admin/controller/ShareAdminListServlet.java
+++ HyeYum/src/admin/controller/ShareAdminListServlet.java
@@ -1,11 +1,18 @@
 package admin.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import admin.model.service.BookAdminService;
+import book.model.vo.BookShare;
+import file.model.service.FileService;
+import file.model.vo.FileData;
 
 /**
  * Servlet implementation class AdminLoginServlet
@@ -26,7 +33,20 @@ public class ShareAdminListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/views/admin/board/shareList.jsp").forward(request, response);
+//		ArrayList<BookShare> rList = new BookAdminService().printAllBookReviewList();
+		FileData fd = new FileData();
+		fd.setFileType("book");
+		ArrayList<FileData> fList = new FileService().printShowReviewFileList();
+//		System.out.println(rList);
+		System.out.println(fList);
+//		if(!rList.isEmpty()) {
+//			request.setAttribute("rList", rList);
+			request.setAttribute("fList", fList);
+			request.getRequestDispatcher("/WEB-INF/views/admin/board/bookList.jsp").forward(request, response);
+			
+//		}else {
+			
+//		}
 	}
 
 	/**
